5ffb0024accfb1f3abe0f3870141501af1e30553
Nicolas Filzi <nfilzi.webservices@gmail.com>
2022-02-21 10:31:05 +0100

    Add ToS page

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
   root to: 'pages#home'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+
+  get 'terms_of_service', to: 'pages#terms_of_service'
 end
